d1689347a459412439e913898167814521155f5e
Eduardo <136937070+Edualnogs@users.noreply.github.com>
2023-08-09 05:52:27 -0300

    Update regressao_linear001.py

--- regressao_linear001.py
+++ regressao_linear001.py
@@ -19,3 +19,15 @@ predictions = model.predict(X_test)
 # Exibindo as previsões
 for i in range(len(X_test)):
     print(f"X_test: {X_test[i]}, Previsão: {predictions[i]}")
+
+
+'''''
+Neste exemplo, estamos utilizando um modelo de regressão linear simples para prever valores com base em uma única variável 
+independente (X). O modelo é treinado com um conjunto de dados de treinamento (X e y). Em seguida, realizamos previsões 
+utilizando um conjunto de dados de teste (X_test) e exibimos os resultados.
+
+É importante ressaltar que este é um exemplo básico e que existem muitas outras técnicas e bibliotecas disponíveis para 
+diferentes tipos de problemas de machine learning em Python. O scikit-learn é uma biblioteca popular e abrangente para 
+aprendizado de máquina em Python, mas também existem outras bibliotecas poderosas, como TensorFlow e PyTorch, que fornecem 
+recursos avançados para tarefas mais complexas.
+'''''
